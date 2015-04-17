24744150760b7c6cc5e361c14fb3c38ddeed20c6
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2015-04-16 21:50:41 -0300

    Modifica las rutas.

--- proyecto-encomiendas/app/controllers/Application.java
+++ proyecto-encomiendas/app/controllers/Application.java
@@ -7,10 +7,6 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Sistema Integrado de Información para Empresas de Transporte"));
-    }
-
     public static Result admin() { return ok(admin.render("Admin")); }
 
     public static Result servicios() { return ok(servicios.render("Admin")); }
