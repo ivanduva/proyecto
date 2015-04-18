4be1248945177cfcddb7682dfe23b30a4f1580f6
Ivan <duva.ivan@gmail.com>
2015-04-18 11:46:55 -0300

    Métodos de PersistenciaDB

--- proyecto-encomiendas/app/dao/PersistenciaDB.java
+++ proyecto-encomiendas/app/dao/PersistenciaDB.java
@@ -1,7 +1,42 @@
 package dao;
 
+
+import play.db.ebean.Model;
+
+import java.util.List;
+
 /**
  * Created by Ivan on 16/02/2015.
  */
-public class PersistenciaDB {
+public class PersistenciaDB implements Persistencia<T,K>{
+    @Override
+    public void create(T t) {
+        T.save();
+    }
+
+    @Override
+    public T get(K k) {
+
+        T t = new Model.Finder(String.class, T.class).byId(K);
+
+        return t;
+    }
+
+    @Override
+    public void update(T t) {
+        T.update();
+    }
+
+    @Override
+    public void delete(T t) {
+        T.delete();
+    }
+
+    @Override
+    public List<T> listAll() {
+
+        List<T> list = new Model.Finder(String.class, T.class).all();
+
+        return list;
+    }
 }
