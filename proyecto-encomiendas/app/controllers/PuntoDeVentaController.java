af4203a8a8adec12cad7d06a408a46dc19204a0a
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2015-05-05 00:52:05 -0300

    Pasa todo a respuestas con json, agrega metodos para armar formulario de punto de venta (necesita refactor)

--- proyecto-encomiendas/app/controllers/PuntoDeVentaController.java
+++ proyecto-encomiendas/app/controllers/PuntoDeVentaController.java
@@ -1,19 +1,23 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import dao.PersistenciaDB;
+import dao.PersistenciaDBLocalidad;
 import dao.PersistenciaDBPuntoDeVenta;
 import dao.PersistenciaDBUsuario;
+import model.Localidad;
 import model.PuntoDeVenta;
-import model.TipoUsuario;
-import model.Usuario;
-import play.data.Form;
+import model.TipoPunto;
+import play.Logger;
+
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import repository.LocalidadRepositorio;
 import repository.PuntoDeVentaRepositorio;
 import repository.UsuarioRepositorio;
 
-
 import java.util.List;
-import views.html.pdv;
 
 import static play.libs.Json.toJson;
 
@@ -24,24 +28,42 @@ public class PuntoDeVentaController extends Controller {
 
     static PuntoDeVentaRepositorio repositorioPdv = new PuntoDeVentaRepositorio(new PersistenciaDBPuntoDeVenta());
     static UsuarioRepositorio repositorioUsuario = new UsuarioRepositorio(new PersistenciaDBUsuario());
+    static LocalidadRepositorio repositorioLocalidad = new LocalidadRepositorio(new PersistenciaDBLocalidad(Localidad.class));
 
     public static Result agregarPunto() {
 
-        PuntoDeVenta punto = Form.form(PuntoDeVenta.class).bindFromRequest().get();
-        Usuario usuario = Form.form(Usuario.class).bindFromRequest().get();
-        usuario.setTipo(TipoUsuario.VENDEDOR);
-        repositorioUsuario.crear(usuario);
-        punto.setUsuario(usuario);
-        repositorioPdv.crear(punto);
-        return listarPuntos();
+//        PuntoDeVenta punto = Json.fromJson(request().body().asJson());
+//        Usuario usuario = Form.form(Usuario.class).bindFromRequest().get();
+//        usuario.setTipo(TipoUsuario.VENDEDOR);
+//        repositorioUsuario.crear(usuario);
+//        punto.setUsuario(usuario);
+//        repositorioPdv.crear(punto);
+
+        JsonNode json = request().body().asJson();
+        Logger.info(json.toString() + "\n");
+        PuntoDeVenta puntoDeVenta = Json.fromJson(json, PuntoDeVenta.class);
+        repositorioPdv.crear(puntoDeVenta);
+        return ok(toJson(puntoDeVenta));
     }
 
     public static Result listarPuntos() {
         List<PuntoDeVenta> puntoDeVentas = repositorioPdv.listarTodo();
-        return ok(pdv.render(puntoDeVentas));
+        return ok(toJson(puntoDeVentas));
     }
 
     public static Result modificarPunto() {
         return ok();
+    }
+
+    // TODO: sacar a controlador de localidades
+    public static Result listarLocalidades() {
+
+        List<Localidad> localidades = repositorioLocalidad.listarTodo();
+        return ok(toJson(localidades));
+    }
+
+    public static Result listarTipoPdv() {
+        TipoPunto[] tiposPunto = TipoPunto.values();
+        return ok(toJson(tiposPunto));
     }
 }
